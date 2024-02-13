a84ca5867eb98658227469159fab77f0841ad427
YuriiTitov <tyv.ura1203@gmail.com>
2024-02-13 21:34:17 +0100

    [impl] results plot

--- 05-bugApp/ui/results_frame.py
+++ 05-bugApp/ui/results_frame.py
@@ -1,5 +1,10 @@
+import tkinter
+
 import customtkinter
 from tktooltip import ToolTip
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from logs_management.bug_logs import mark_detected_bugs
 from properties_management.manage_bugs import get_activated_bugs
@@ -16,33 +21,57 @@ class ResultsFrame(customtkinter.CTkScrollableFrame):
         self.configure(border_color="#323232")
         self.padx = 8
 
+        self.left_side_panel = customtkinter.CTkFrame(self, width=700, corner_radius=8, fg_color="#C1CDCD")
+        self.left_side_panel.pack(side=tkinter.LEFT, fill=tkinter.Y, expand=False, padx=25, pady=10)
+
+        self.right_side_panel = customtkinter.CTkFrame(self, corner_radius=8, fg_color="#212121")
+        self.right_side_panel.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True, padx=0, pady=0)
+        self.right_side_panel.configure(border_width=1)
+        self.right_side_panel.configure(border_color="#323232")
+
+        self.activated_bugs = mark_detected_bugs(get_activated_bugs())
+
         self.create_title_row()
         self.create_fields()
+        self.create_statistics()
 
     def create_title_row(self):
-        name = customtkinter.CTkLabel(self, text="Activated Bugs", text_color="black")
+        name = customtkinter.CTkLabel(self.left_side_panel, text="Activated Bugs", text_color="black")
         name.grid(row=0, column=0, padx=15, pady=15)
 
-        value = customtkinter.CTkLabel(self, text="Is Detected?", text_color="black")
+        value = customtkinter.CTkLabel(self.left_side_panel, text="Is Detected?", text_color="black")
         value.grid(row=0, column=1, padx=15, pady=15)
 
     def create_bug_field(self, bug_config, current_row):
-        label = customtkinter.CTkLabel(self, text=bug_config.name, text_color="black")
+        label = customtkinter.CTkLabel(self.left_side_panel, text=bug_config.name, text_color="black")
         label.grid(row=current_row, column=0, padx=15, pady=15)
 
         detected_label_color = "green" if bug_config.is_detected else "red"
-        detected_label = customtkinter.CTkLabel(self, text=bug_config.is_detected, text_color=detected_label_color)
+        detected_label = customtkinter.CTkLabel(self.left_side_panel, text=bug_config.is_detected, text_color=detected_label_color)
         detected_label.grid(row=current_row, column=1, padx=15, pady=15)
 
         ToolTip(label, msg=bug_config.description, delay=0.01,
                 fg="#ffffff", bg="#1c1c1c", padx=10, pady=10)
 
     def create_fields(self):
-        activated_bugs = get_activated_bugs()
-        activated_bugs = mark_detected_bugs(activated_bugs)
-
         row_number = 1
-        for bug in activated_bugs:
+        for bug in self.activated_bugs:
             self.create_bug_field(bug, row_number)
             row_number += 1
+
+    def create_statistics(self):
+        activated_bugs_number = len(self.activated_bugs)
+        detected_bugs_number = sum(1 for bug in self.activated_bugs if bug.is_detected)
+        detected_percentage = round(detected_bugs_number/activated_bugs_number * 100, 2)
+
+        colors = ["blue", "green"]
+
+        fig, ax = plt.subplots()
+
+        ax.bar(["Activated bugs", "Detected bugs"], [activated_bugs_number, detected_bugs_number], color=colors)
+        ax.text(1, detected_bugs_number+0.5, f'{detected_percentage}%', ha='center')
+
+        canvas = FigureCanvasTkAgg(fig, master=self.right_side_panel)
+        canvas.draw()
+        canvas.get_tk_widget().pack(padx=10, pady=10)
 
